ed7c64ba706403b5b839b255090a9c0f825b21d2
josemoncada87 <josemoncada87@hotmail.com>
2024-04-01 09:38:43 -0500

    update pre-hooks

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@
  * or add some configurations to the server
  */
 
-const app = require('./index.js');
+const app = require('./index');
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('server starting 🚀🆙✔ on port 3000!'));
